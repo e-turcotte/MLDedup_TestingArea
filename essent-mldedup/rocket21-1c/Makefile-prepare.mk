1ac67cc2e02555ad0bf51d0f422da102de7b4e7b
Yash Mittal <yashkmittal@utexas.edu>
2026-03-29 20:21:27 -0500

    [fix] added one compile step

--- essent-mldedup/rocket21-1c/Makefile-prepare.mk
+++ essent-mldedup/rocket21-1c/Makefile-prepare.mk
@@ -1,5 +1,7 @@
 include Makefrag-variables.mk
 
+RISCV_ISA_SIM_REPO ?= https://github.com/riscv-software-src/riscv-isa-sim.git
+
 
 $(FIR_PATH):
 	tar -xvf ./firrtl.tar.gz
@@ -7,8 +9,9 @@ $(FIR_PATH):
 
 .PHONY: all clean
 
-all: $(FIR_PATH) 
-	cd riscv-isa-sim; git checkout `cat ../riscv-isa-sim.commit`
+all: $(FIR_PATH)
+	test -d riscv-isa-sim/.git || git clone -q -- "$(RISCV_ISA_SIM_REPO)" riscv-isa-sim
+	cd riscv-isa-sim && (git fetch -q --tags 2>/dev/null || true) && git checkout $$(cat ../riscv-isa-sim.commit)
 
 clean:
 	rm $(FIR_PATH)
